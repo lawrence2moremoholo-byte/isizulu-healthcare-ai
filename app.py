413c3f660e1397c08d5fe2484ae6001e4e9c44e1
lawrence2moremoholo-byte <lawrence2.moremoholo@gmail.com>
2025-09-25 18:49:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -139,12 +139,6 @@ def login():
     
     return render_template('login.html')
 
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('login'))
-
 @app.route('/dashboard')
 @login_required
 def dashboard():
